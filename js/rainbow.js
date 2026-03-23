79f2889586cb62a572db7e959b03079d1e18a1d3
Sam <seifi131@icloud.com>
2026-03-23 01:19:04 +0000

    Add rainbow effect with random color changes

---
+++ js/rainbow.js
@@ -0,0 +1,16 @@
+setInterval(rainbow, 1000);
+function rainbow() {
+    // i.c. random color
+    var ic = document.getElementById('rainbow');
+    ic.style.color = getRandomColor();
+}
+
+// random color function, returns a random hex
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
